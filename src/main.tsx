502ad48e7c140d60992f76235b8d53a474dc54c6
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2024-12-21 16:22:21 +0530

    feat: fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
 import { router } from './routes';
 import './index.css';
+import { HomePage } from './pages/HomePage';
+import { ProjectPage } from './pages/ProjectPage';
+import { BottleSpinGame } from './pages/games/BottleSpinGame';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <BrowserRouter basename='MiniProjects'>
+        <div className="mx-auto">
+          <Routes>
+          <Route path='/' element={<HomePage />} index/>
+          <Route path='/project/:id' element={<ProjectPage />} />
+          <Route path='/games/bottle' element={<BottleSpinGame />} />
+          </Routes>
+        </div>
+        </BrowserRouter>
+   </StrictMode>
 );
